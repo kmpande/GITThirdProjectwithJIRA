d8b07282bedccecf8972ee0ec362c1de8b881e81
kmpande <deepali.wajpe@gmail.com>
2022-08-12 23:00:39 +0530

    SBCWR-9:Create Configuration

---
+++ src/main/java/com/HiberwithREST/service/EmployeeServiceI.java
@@ -0,0 +1,11 @@
+package com.HiberwithREST.service;
+
+import java.util.List;
+
+import com.HiberwithREST.model.Employee;
+
+public interface EmployeeServiceI {
+public Employee saveEmployee();
+public List<Employee> getAllEmployee();
+
+}
---
+++ src/main/java/com/HiberwithREST/service/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.HiberwithREST.service;
+
+import java.util.List;
+
+import com.HiberwithREST.model.Employee;
+
+public class EmployeeServiceImpl implements EmployeeServiceI{
+
+	@Override
+	public Employee saveEmployee() {
+	
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		
+		return null;
+	}
+
+}
